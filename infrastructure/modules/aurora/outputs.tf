f6b519e353e9e6d0389ea42bd1e9c68c36f14580
JamesRuane-is <108880654+JamesRuane-is@users.noreply.github.com>
2026-06-09 10:06:08 +0100

    [URP-261] create aurora terraform module (#5)
    Co-authored-by: SmithR-IS <ryan.smith@informed.com>

---
+++ infrastructure/modules/aurora/outputs.tf
@@ -0,0 +1,44 @@
+output "cluster_arn" {
+  description = "ARN of the Aurora cluster"
+  value       = aws_rds_cluster.aurora.arn
+}
+
+output "cluster_endpoint" {
+  description = "Aurora cluster writer endpoint"
+  value       = aws_rds_cluster.aurora.endpoint
+}
+
+output "cluster_identifier" {
+  description = "Identifier of the Aurora cluster"
+  value       = aws_rds_cluster.aurora.cluster_identifier
+}
+
+output "database_name" {
+  description = "Name of the Aurora database"
+  value       = aws_rds_cluster.aurora.database_name
+}
+
+output "master_user_secret_arn" {
+  description = "ARN of the Secrets Manager secret containing the managed master user password"
+  value       = try(aws_rds_cluster.aurora.master_user_secret[0].secret_arn, null)
+}
+
+output "port" {
+  description = "Port used by the Aurora cluster"
+  value       = aws_rds_cluster.aurora.port
+}
+
+output "reader_endpoint" {
+  description = "Aurora cluster reader endpoint"
+  value       = aws_rds_cluster.aurora.reader_endpoint
+}
+
+output "security_group_id" {
+  description = "ID of the Aurora security group"
+  value       = aws_security_group.aurora_postgres_sg.id
+}
+
+output "subnet_group_name" {
+  description = "Name of the Aurora DB subnet group"
+  value       = aws_db_subnet_group.aurora.name
+}
