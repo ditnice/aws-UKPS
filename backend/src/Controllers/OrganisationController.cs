22ab5fdb787f65ee07bb93dfc54b765d84aefcd0
JacobSilasBentley <jacob.bentley@informed.com>
2026-07-07 16:01:53 +0100

    UKPS 282 - added an endpoint to allow for the deactivation of a membership.

--- backend/src/Controllers/OrganisationController.cs
+++ backend/src/Controllers/OrganisationController.cs
@@ -59,10 +59,37 @@ public class OrganisationController(IOrganisationService organisationService) : 
         );
     }
 
-    [HttpPatch("{organisationId:int}/memberships/{membershipId}")]
+    [HttpPatch("{organisationId:int}/memberships/{membershipId}/deactivate")]
     [ProducesResponseType<OrganisationMembershipDto>(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<OrganisationMembershipDto>> DeactivateMembership(
+        int organisationId,
+        int membershipId,
+        CancellationToken cancellationToken
+    )
+    {
+        var result = await organisationService.Memberships.DeactivateMembership(
+            organisationId,
+            membershipId,
+            cancellationToken
+        );
+
+        return result.Match<ActionResult<OrganisationMembershipDto>>(
+            x => Ok(x),
+            x =>
+                x switch
+                {
+                    OrganisationMembershipDeactivateUserError.NotFound => NotFound(
+                        $"Could not find a membership with organisation ID = {organisationId} and membership ID = {membershipId}."
+                    ),
+                    _ => throw new UnreachableException(),
+                }
+        );
+    }
+
+    [HttpPatch("{organisationId:int}/memberships/{membershipId}/update-role")]
+    [ProducesResponseType<OrganisationMembershipDto>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<OrganisationMembershipDto>> UpdateUserRole(
         int organisationId,
         int membershipId,
@@ -82,7 +109,7 @@ public class OrganisationController(IOrganisationService organisationService) : 
                 x switch
                 {
                     OrganisationMembershipUpdateUserRoleError.NotFound notFound => NotFound(
-                        $"Could not find a membership with organisation ID = {notFound.OrganisationId} and membership ID = {notFound.MembershipId}"
+                        $"Could not find a membership with organisation ID = {notFound.OrganisationId} and membership ID = {notFound.MembershipId}."
                     ),
                     _ => throw new UnreachableException(),
                 }
