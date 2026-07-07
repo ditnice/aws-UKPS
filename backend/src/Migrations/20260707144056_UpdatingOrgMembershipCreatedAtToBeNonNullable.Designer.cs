9d9a264813f4b38045aceea82a56dd781d5bbbbf
JacobSilasBentley <jacob.bentley@informed.com>
2026-07-07 15:42:16 +0100

    UKPS 283 - Added migration for changes to CreatedAt in the user org membership entity.

--- backend/src/Migrations/20260707144056_UpdatingOrgMembershipCreatedAtToBeNonNullable.Designer.cs
+++ backend/src/Migrations/20260707144056_UpdatingOrgMembershipCreatedAtToBeNonNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UKPS.Api.Data;
@@ -12,9 +13,11 @@ using UKPS.Api.Data;
 namespace UKPS.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260707144056_UpdatingOrgMembershipCreatedAtToBeNonNullable")]
+    partial class UpdatingOrgMembershipCreatedAtToBeNonNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
