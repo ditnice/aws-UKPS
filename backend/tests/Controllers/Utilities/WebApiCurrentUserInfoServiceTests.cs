114028fd84b082c81b717e9f9914b43dafb116ae
JacobSilasBentley <jacob.bentley@informed.com>
2026-07-16 15:03:42 +0100

    UKPS-306 - Tidied up tests removing unecessary tests.

--- backend/tests/Controllers/Utilities/WebApiCurrentUserInfoServiceTests.cs
+++ backend/tests/Controllers/Utilities/WebApiCurrentUserInfoServiceTests.cs
@@ -18,17 +18,14 @@ public class WebApiCurrentUserInfoServiceTests
         var faker = new Faker();
         foreach (var userRole in Enum.GetValues<UserRole>())
         {
-            // Arrange
             var organisationId = faker.Random.Int(min: 0);
             var service = CreateService(
                 new Claim(UkpsClaimTypes.OrganisationId, $"{organisationId}"),
                 new Claim(UkpsClaimTypes.UserRole, userRole.ToString())
             );
 
-            // Act
             CurrentUser result = service.GetCurrentUserInfo();
 
-            // Assert
             result.OrganisationId.ShouldBe(organisationId);
             result.UserRole.ShouldBe(userRole);
         }
@@ -37,15 +34,12 @@ public class WebApiCurrentUserInfoServiceTests
     [Fact]
     public void GetCurrentUserInfo_ShouldThrow_WhenOrganisationIdClaimIsMissing()
     {
-        // Arrange
         var service = CreateService(
             new Claim(UkpsClaimTypes.UserRole, UserRole.Champion.ToString())
         );
 
-        // Act
         var exception = Should.Throw<InvalidOperationException>(() => service.GetCurrentUserInfo());
 
-        // Assert
         exception.Message.ShouldBe($"Invalid {UkpsClaimTypes.OrganisationId} claim value.");
     }
 
@@ -55,61 +49,48 @@ public class WebApiCurrentUserInfoServiceTests
     [InlineData("1.5")]
     public void GetCurrentUserInfo_ShouldThrow_WhenOrganisationIdClaimIsInvalid(string claimValue)
     {
-        // Arrange
         var service = CreateService(
             new Claim(UkpsClaimTypes.OrganisationId, claimValue),
             new Claim(UkpsClaimTypes.UserRole, UserRole.Super.ToString())
         );
 
-        // Act
         var exception = Should.Throw<InvalidOperationException>(() => service.GetCurrentUserInfo());
 
-        // Assert
         exception.Message.ShouldBe($"Invalid {UkpsClaimTypes.OrganisationId} claim value.");
     }
 
     [Fact]
     public void GetCurrentUserInfo_ShouldThrow_WhenUserRoleClaimIsMissing()
     {
-        // Arrange
         var service = CreateService(new Claim(UkpsClaimTypes.OrganisationId, "123"));
 
-        // Act
         var exception = Should.Throw<InvalidOperationException>(() => service.GetCurrentUserInfo());
 
-        // Assert
         exception.Message.ShouldBe($"Invalid {UkpsClaimTypes.UserRole} claim value.");
     }
 
     [Theory]
-    // [InlineData("")]
-    // [InlineData("Administrator")]
     [InlineData("123")]
     public void GetCurrentUserInfo_ShouldThrow_WhenUserRoleClaimIsInvalid(string claimValue)
     {
-        // Arrange
         var service = CreateService(
             new Claim(UkpsClaimTypes.OrganisationId, "123"),
             new Claim(UkpsClaimTypes.UserRole, claimValue)
         );
 
-        // Act
         var exception = Should.Throw<InvalidOperationException>(() => service.GetCurrentUserInfo());
 
-        // Assert
         exception.Message.ShouldBe($"Invalid {UkpsClaimTypes.UserRole} claim value.");
     }
 
     [Fact]
     public void GetCurrentUserInfo_ShouldThrow_WhenHttpContextIsNull()
     {
-        // Arrange
         var accessor = Substitute.For<IHttpContextAccessor>();
         accessor.HttpContext.Returns((HttpContext?)null);
 
         var service = new WebApiCurrentUserInfoService(accessor);
 
-        // Act
         Should.Throw<InvalidOperationException>(() => service.GetCurrentUserInfo());
     }
 
