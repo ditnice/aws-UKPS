df7c65736b9db232c2ed9470433b5eb15db6ee89
JacobSilasBentley <jacob.bentley@informed.com>
2026-07-22 14:28:31 +0100

    UKPS-394 - Updated grammar of the user sign up request email.

--- backend/src/Application/Users/UserSignUpRequestEmail.cs
+++ backend/src/Application/Users/UserSignUpRequestEmail.cs
@@ -5,6 +5,7 @@ namespace UKPS.Api.Application.Users;
 internal class UserSignUpRequestEmail : IEmail
 {
     public string Subject => "UKPS Sign Up Request";
-    public string Content => $"Hello, some wants to create an account. Click this link: {Link}";
+    public string Content =>
+        $"Hello, someone wants you to create an account. Click this link: {Link}";
     public required string Link { get; init; }
 }
