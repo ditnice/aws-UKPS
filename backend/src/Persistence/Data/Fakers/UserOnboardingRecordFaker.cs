6558ebf17488c2a38954a5ac387315f75953da30
JacobSilasBentley <30935918+JacobSilasBentley@users.noreply.github.com>
2026-07-29 10:49:01 +0100

    [URP-395] - User Setup Service Layer (#130)
    Co-authored-by: JacobSilasBentley <jacob.bentley@informed.com>

--- backend/src/Persistence/Data/Fakers/UserOnboardingRecordFaker.cs
+++ backend/src/Persistence/Data/Fakers/UserOnboardingRecordFaker.cs
@@ -7,11 +7,13 @@ internal sealed class UserOnboardingRecordFaker : Faker<UserOnboardingRecord>
 {
     public UserOnboardingRecordFaker()
     {
-        RuleFor(x => x.SetupToken, f => Guid.NewGuid());
+        UseSeed(15); // Random seed
+
+        RuleFor(x => x.SetupToken, f => f.Random.Guid());
         RuleFor(x => x.UserEmail, f => f.Internet.Email());
         RuleFor(x => x.CreatedAt, f => f.Date.RecentOffset(30).UtcDateTime);
         RuleFor(x => x.CreatedBy, f => f.Internet.UserName());
-        RuleFor(x => x.NewUserOrganisationId, f => f.Random.Long(1, long.MaxValue));
+        RuleFor(x => x.NewUserOrganisationId, f => 0);
         RuleFor(x => x.NewUserOrganisation, _ => null);
     }
 }
