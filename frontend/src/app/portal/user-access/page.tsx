d63466ca6fa7c0dae77e543387e6426047d68c1f
EmmaFuller437 <emma.fuller@informed.com>
2026-08-12 14:24:27 +0100

    Fixed typo

--- frontend/src/app/portal/user-access/page.tsx
+++ frontend/src/app/portal/user-access/page.tsx
@@ -15,7 +15,7 @@ export default function RequestSumbitted() {
       <p>julie.brooks@example.com is a standard user.</p>
       <p>Choose what you want to do:</p>
       <ul className={styles.marginBottom}>
-        <li>Change permissions - chnage what the user can do.</li>
+        <li>Change permissions - change what the user can do.</li>
         <li>
           Deactivate user - temporarily stop the user&#39;s access. You can reactivate them later.
         </li>
