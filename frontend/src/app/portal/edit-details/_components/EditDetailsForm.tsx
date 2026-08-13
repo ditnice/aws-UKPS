627c95ab401fe6a5ca07a3582b9c0505719b7c14
EmmaFuller437 <emma.fuller@informed.com>
2026-08-13 10:34:08 +0100

    edit details screen

---
+++ frontend/src/app/portal/edit-details/_components/EditDetailsForm.tsx
@@ -0,0 +1,133 @@
+'use client'
+import { revalidateLogic, useForm } from '@tanstack/react-form'
+import { ChangeEvent } from 'react'
+import { z } from 'zod'
+
+import { Button } from '@nice-digital/nds-button'
+
+import { getFieldErrorMessage } from '@/components/Form/getFieldErrorMessage'
+import { Input } from '@/components/Input/Input'
+
+import styles from './EditDetailsForm.module.scss'
+
+const EditDetails = z.object({
+  fullName: z.string().trim().min(1, 'Enter your full name'),
+  workEmail: z
+    .string()
+    .trim()
+    .min(1, 'Enter your work email address')
+    .pipe(z.email('Enter an email address in the correct format, like name@example.com')),
+  phoneNumber: z.string().trim().min(1, 'Enter your phone number'),
+})
+
+type EditDetailsValues = z.input<typeof EditDetails>
+
+export function EditDetailsForm() {
+  const form = useForm({
+    defaultValues: {
+      fullName: 'Julie Brooks', // These default values will be from their existing account
+      workEmail: 'admin@bigpharma1.com',
+      phoneNumber: '01234567890',
+    } satisfies EditDetailsValues,
+    validationLogic: revalidateLogic({
+      mode: 'submit',
+      modeAfterSubmission: 'blur',
+    }),
+    validators: {
+      onDynamic: EditDetails,
+    },
+    onSubmit: ({ value }) => {
+      EditDetails.parse(value)
+    },
+  })
+  return (
+    <form
+      noValidate
+      onSubmit={(event) => {
+        event.preventDefault()
+        event.stopPropagation()
+        void form.handleSubmit()
+      }}
+    >
+      <div className={styles.marginBottom}>
+        <form.Field name="fullName">
+          {(field) => {
+            const errorMessage = getFieldErrorMessage(field.state.meta.errors)
+            return (
+              <Input
+                autoComplete="name"
+                error={Boolean(errorMessage)}
+                errorMessage={errorMessage}
+                label="Full name"
+                name={field.name}
+                onBlur={field.handleBlur}
+                onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                  field.handleChange(event.target.value)
+                }
+                type="text"
+                value={field.state.value}
+                width="one-third"
+                className={styles.marginBottom}
+              />
+            )
+          }}
+        </form.Field>
+      </div>
+      <div className={styles.marginBottom}>
+        <form.Field name="workEmail">
+          {(field) => {
+            const errorMessage = getFieldErrorMessage(field.state.meta.errors)
+            return (
+              <Input
+                autoComplete="email"
+                error={Boolean(errorMessage)}
+                errorMessage={errorMessage}
+                label="Work email address"
+                name={field.name}
+                onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                  field.handleChange(event.target.value)
+                }
+                onBlur={field.handleBlur}
+                type="email"
+                value={field.state.value}
+                width="one-third"
+                className={styles.marginBottom}
+              />
+            )
+          }}
+        </form.Field>
+      </div>
+      <div className={styles.marginBottom}>
+        <form.Field name="phoneNumber">
+          {(field) => {
+            const errorMessage = getFieldErrorMessage(field.state.meta.errors)
+
+            return (
+              <Input
+                autoComplete="phone number"
+                error={Boolean(errorMessage)}
+                errorMessage={errorMessage}
+                label="Contact number"
+                name={field.name}
+                onBlur={field.handleBlur}
+                onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                  field.handleChange(event.target.value)
+                }
+                hint="For international numbers include the country code."
+                width="one-third"
+                value={field.state.value}
+                className={styles.marginBottom}
+              />
+            )
+          }}
+        </form.Field>
+      </div>
+      <div className={styles.buttonGap}>
+        <Button type="submit" variant="cta">
+          Save
+        </Button>
+        <Button variant="secondary"> Cancel</Button>
+      </div>
+    </form>
+  )
+}
