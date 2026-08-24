1f4138aa313edcd3ac0d5cd4e945388d6ef67717
JacobSilasBentley <jacob.bentley@informed.com>
2026-08-24 15:50:31 +0100

    Updated appsetting development to be tracked.

--- backend/src/Persistence/DatabaseConfiguration.cs
+++ backend/src/Persistence/DatabaseConfiguration.cs
@@ -3,8 +3,8 @@ namespace UKPS.Api.Persistence;
 internal sealed record DatabaseConfiguration
 {
     public const string SectionName = "Database";
-    public string? Host { get; init; }
     public bool MigrateOnStartup { get; init; }
+    public string? Host { get; init; }
     public string? Name { get; init; }
     public string? Password { get; init; }
     public int? Port { get; init; }
