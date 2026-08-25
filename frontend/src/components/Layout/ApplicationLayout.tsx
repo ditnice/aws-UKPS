31e868d0692be4df26a2db5acdb76732d0c36b1f
JacobSilasBentley <30935918+JacobSilasBentley@users.noreply.github.com>
2026-08-25 07:36:53 +0100

    Updated the footer so it always at the bottom (#251)
    Co-authored-by: JacobSilasBentley <jacob.bentley@informed.com>
    Co-authored-by: SmithR-IS <ryan.smith@informed.com>

--- frontend/src/components/Layout/ApplicationLayout.tsx
+++ frontend/src/components/Layout/ApplicationLayout.tsx
@@ -2,6 +2,7 @@
 
 import { Container } from '@nice-digital/nds-container'
 
+import styles from './ApplicationLayout.module.scss'
 import { Footer } from './Footer/Footer'
 import { Header } from './Header/Header'
 import { Main } from './Main/Main'
@@ -11,13 +12,13 @@ import type { ReactNode } from 'react'
 export function ApplicationLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <Header skipLinkId="content-start" />
-
-      <Main id="content-start">
-        <Container>{children}</Container>
-      </Main>
-
-      <Footer />
+      <div className={styles.contentWrapper}>
+        <Header skipLinkId="content-start" />
+        <Main className={styles.mainContent} id="content-start">
+          <Container>{children}</Container>
+        </Main>
+        <Footer />
+      </div>
     </>
   )
 }
