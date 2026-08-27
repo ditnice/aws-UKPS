66322085f5e4e9b215c9d605852e55ca75be04a2
JacobSilasBentley <jacob.bentley@informed.com>
2026-08-27 14:43:12 +0100

    UKPS-438 - Updated error state to have styling.

--- frontend/src/components/Placeholder/ErrorState.tsx
+++ frontend/src/components/Placeholder/ErrorState.tsx
@@ -1,5 +1,3 @@
-// This is a deliberately unstyled empty component to track
-// where we might need to update at a later date to show an error state
 export const ErrorState = (props: React.PropsWithChildren) => {
-  return <>{props.children}</>
+  return <p role="alert">{props.children}</p>
 }
