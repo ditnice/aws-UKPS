1ecfde31f9270f8896b84dac64e24b181d5f0a73
JacobSilasBentley <jacob.bentley@informed.com>
2026-08-28 10:23:12 +0100

    UKPS-438 - Added a default username claim.

--- backend/src/WebApi/InternalServices/Authentication/DevAuthenticationClaims.cs
+++ backend/src/WebApi/InternalServices/Authentication/DevAuthenticationClaims.cs
@@ -12,6 +12,7 @@ internal class DevAuthenticationClaims
         new Claim(UkpsClaimTypes.UserRole, UserRole.Super.ToString()),
         new Claim(UkpsClaimTypes.OrganisationId, $"{1}"),
         new Claim(UkpsClaimTypes.Email, DefaultUserEmail),
+        new Claim(UkpsClaimTypes.Username, "14f9bc9b-ada7-4f1c-9f5c-a4e7de72d80d"),
     ];
 
     public ICollection<Claim> Claims { get; } = DefaultClaims.ToList();
