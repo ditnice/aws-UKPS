8cc87ac7f9876fc7537cf26781934e6d1e169d83
JacobSilasBentley <30935918+JacobSilasBentley@users.noreply.github.com>
2026-09-01 14:35:32 +0100

    [URP-438] - Tidying up telphone number faking and validation (#262)
    Co-authored-by: JacobSilasBentley <jacob.bentley@informed.com>

--- backend/src/WebApi/Validators/PhoneNumberAttribute.cs
+++ backend/src/WebApi/Validators/PhoneNumberAttribute.cs
@@ -6,6 +6,12 @@ namespace UKPS.Api.WebApi.Validators;
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
 internal sealed class PhoneNumberAttribute : ValidationAttribute
 {
+    public PhoneNumberAttribute()
+        : base() { }
+
+    public PhoneNumberAttribute(string errorMessage)
+        : base(errorMessage: errorMessage) { }
+
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
         if (value is string str && PhoneNumberValidator.IsValid(str))
@@ -13,6 +19,9 @@ internal sealed class PhoneNumberAttribute : ValidationAttribute
             return ValidationResult.Success!;
         }
 
-        return new ValidationResult(ErrorMessage ?? "Value must be a valid phone number.");
+        return new ValidationResult(
+            ErrorMessage ?? "Value must be a valid phone number.",
+            validationContext.MemberName is null ? null : [validationContext.MemberName]
+        );
     }
 }
