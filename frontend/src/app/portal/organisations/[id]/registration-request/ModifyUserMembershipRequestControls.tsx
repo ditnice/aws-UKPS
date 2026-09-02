af589aa49a28c709bbf9b49138f8ba1c1ddb95bb
JacobSilasBentley <jacob.bentley@informed.com>
2026-09-02 11:21:33 +0100

    UKPS-313 - Fixed automated tests.

--- frontend/src/app/portal/organisations/[id]/registration-request/ModifyUserMembershipRequestControls.tsx
+++ frontend/src/app/portal/organisations/[id]/registration-request/ModifyUserMembershipRequestControls.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
-import { approve, reject } from '@/client/generated/sdk.gen'
+import { approve, reject } from '@/client/generated'
 import { Button, ButtonGroup } from '@/components/Button/Button'
 import { ErrorState } from '@/components/Placeholder/ErrorState'
 
