93f6330608abc968c2e80bb349566e913b90d3cc
JamesRuane-is <james.ruane@informed.com>
2026-09-02 12:16:29 +0100

    [URP-433] add lambda logs to kms arns

--- infrastructure/modules/lambda/dbMigrator/cloudwatch.tf
+++ infrastructure/modules/lambda/dbMigrator/cloudwatch.tf
@@ -4,7 +4,7 @@ resource "aws_cloudwatch_log_group" "db_migrator_log_group" {
   kms_key_id        = var.cloudwatch_kms_arn
 
   tags = merge(var.tags, {
-    Name        = "${var.project}-${var.environment}-${var.service_name}-ecs-logs"
+    Name        = "${var.project}-${var.environment}-${var.service_name}-lambda-logs"
     Environment = var.environment
     Project     = var.project
     Service     = var.service_name
