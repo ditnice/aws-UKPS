a6d528bcfbf604e4e81f1f8663cf9054fe19d875
SmithR-IS <ryan.smith@informed.com>
2026-09-03 16:34:33 +0100

    Fixed Lambda SG rules and namings

--- infrastructure/modules/lambda/dbMigrator/security_groups.tf
+++ infrastructure/modules/lambda/dbMigrator/security_groups.tf
@@ -1,8 +1,14 @@
-resource "aws_security_group" "this" {
-  name        = "${local.function_name}-sg"
-  description = "Security group for the ${local.function_name} Lambda"
+resource "aws_security_group" "lambda_sg" {
+  description = "Security group for the ${var.project}-${var.environment} backend migration Lambda"
+  name        = "${var.project}-${var.environment}-${var.service_name}-sg"
   vpc_id      = var.vpc_id
-  tags        = var.tags
+
+  tags = merge(var.tags, {
+    Name        = "${var.project}-${var.environment}-${var.service_name}-lamda-sg"
+    Environment = var.environment
+    Project     = var.project
+    Service     = var.service_name
+  })
 }
 
 resource "aws_vpc_security_group_egress_rule" "aurora" {
@@ -10,7 +16,7 @@ resource "aws_vpc_security_group_egress_rule" "aurora" {
   from_port                    = var.db_port
   to_port                      = var.db_port
   ip_protocol                  = "tcp"
-  security_group_id            = aws_security_group.this.id
+  security_group_id            = aws_security_group.lambda_sg.id
   referenced_security_group_id = var.db_security_group_id
 }
 
@@ -19,7 +25,7 @@ resource "aws_vpc_security_group_egress_rule" "secrets_manager" {
   description       = "Allow Lambda to reach Secrets Manager via HTTPS"
   from_port         = 443
   ip_protocol       = "tcp"
-  security_group_id = aws_security_group.this.id
+  security_group_id = aws_security_group.lambda_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   to_port           = 443
 }
