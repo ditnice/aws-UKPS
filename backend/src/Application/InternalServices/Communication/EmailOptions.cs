5de158f82659b4398bbcd1fb2a73d309c1dad320
JacobSilasBentley <jacob.bentley@informed.com>
2026-09-07 13:12:28 +0100

    UKPS-460 - Updated the upper limit fo the QueueWaitTimeInSeconds to be 20 inline with AWS documentation.

--- backend/src/Application/InternalServices/Communication/EmailOptions.cs
+++ backend/src/Application/InternalServices/Communication/EmailOptions.cs
@@ -15,6 +15,6 @@ internal record EmailOptions
     [Range(1, 10)]
     public int QueueMaxNumberOfMessages { get; init; } = 1;
 
-    [Range(0, 120)]
+    [Range(0, 20)]
     public int QueueWaitTimeInSeconds { get; init; } = 20;
 }
