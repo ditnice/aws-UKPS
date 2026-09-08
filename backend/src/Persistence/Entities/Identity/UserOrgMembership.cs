3b54353d9ddf81d5607e0facfacf0849299d2f72
JacobSilasBentley <30935918+JacobSilasBentley@users.noreply.github.com>
2026-09-08 09:39:45 +0100

    [URP-309] - Deactivate user front end (#310)
    Co-authored-by: JacobSilasBentley <jacob.bentley@informed.com>

--- backend/src/Persistence/Entities/Identity/UserOrgMembership.cs
+++ backend/src/Persistence/Entities/Identity/UserOrgMembership.cs
@@ -44,4 +44,60 @@ internal sealed class UserOrgMembership
         UserOrgStatus[] authorisedStatuses = [UserOrgStatus.Active, UserOrgStatus.Inactive];
         return authorisedStatuses.Contains(Status);
     }
+
+    public IEnumerable<UserMembershipAction> GetPermittedActions(
+        CognitoUsername currentUserUsername,
+        UserRole currentUserRole
+    )
+    {
+        if (User is null)
+        {
+            throw new InvalidOperationException(
+                "The User navigation property must be loaded before calling GetPermittedActions."
+            );
+        }
+
+        if (currentUserUsername == User.CognitoUsername)
+        {
+            return [];
+        }
+
+        if (currentUserRole is not (UserRole.Champion or UserRole.Super))
+        {
+            return [];
+        }
+
+        UserMembershipAction[] nonCommandRelatedActions =
+            Status == UserOrgStatus.Active || Status == UserOrgStatus.Inactive
+                ? [UserMembershipAction.EditUserRole]
+                : [];
+        IEnumerable<UserOrgMembershipStateMachine.Command> permittedCommands =
+            _statusManager.GetPermittedStateChangingCommands();
+        return GetActionsFromPermittedCommands(permittedCommands).Concat(nonCommandRelatedActions);
+    }
+
+    private static IEnumerable<UserMembershipAction> GetActionsFromPermittedCommands(
+        IEnumerable<UserOrgMembershipStateMachine.Command> permittedCommands
+    )
+    {
+        return permittedCommands.Select(ConvertCommandToAction).OfType<UserMembershipAction>();
+    }
+
+    private static UserMembershipAction? ConvertCommandToAction(
+        UserOrgMembershipStateMachine.Command x
+    )
+    {
+        return x switch
+        {
+            UserOrgMembershipStateMachine.Command.AccessGranted =>
+                UserMembershipAction.ApproveMembership,
+            UserOrgMembershipStateMachine.Command.RequestRejected =>
+                UserMembershipAction.RejectMembership,
+            UserOrgMembershipStateMachine.Command.Deactivate =>
+                UserMembershipAction.DeactivateMembership,
+            UserOrgMembershipStateMachine.Command.Reactivate =>
+                UserMembershipAction.ReactivateMembership,
+            _ => null,
+        };
+    }
 }
